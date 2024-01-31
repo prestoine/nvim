7d0251e0a4c4845cfa83bb23167c0922886f6baa
desouzapreston <desouzapreston@gmail.com>
2024-01-31 01:10:10 -0600

    update keybindings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -20,9 +20,9 @@ map('n', '<Leader>j', '<C-w>j', opts)
 map('n', '<Leader>k', '<C-w>k', opts)
 
 -- Save and Quit
-map('n', '<Leader>s', ':w<CR>', opts)
-map('n', '<Leader>q', ':q<CR>', opts)
-map('n', '<Leader>qq', ':wa<CR>:qa<CR>', opts)
+map('n', '<Leader>s', ':w<CR>', opts)  -- Save
+map('n', '<Leader>q', ':q<CR>', opts)  -- Quit current window
+map('n', '<Leader>Q', ':wa<CR>:qa<CR>', opts)  -- Save all and quit
 
 -- Telescope key mappings
 map('n', '<Leader>ff', ':Telescope find_files<CR>', opts)
@@ -30,31 +30,25 @@ map('n', '<Leader>fg', ':Telescope live_grep<CR>', opts)
 map('n', '<Leader>fb', ':Telescope buffers<CR>', opts)
 map('n', '<Leader>fh', ':Telescope help_tags<CR>', opts)
 
---- File and content search using Telescope
-map('n', '<Leader>fs', ':Telescope find_files<CR>', opts)
-map('n', '<Leader>cs', ':Telescope live_grep<CR>', opts)
-
-
 -- Open a new empty buffer and prompt for a filename to save
 map('n', '<Leader>nf', '', { noremap = true, silent = true, callback = create_and_save_new_file })
 
 -- Open a new tab and split windows
-map('n', '<Leader>nt', ':tabnew<CR>', opts)
-map('n', '<Leader>hw', ':split<CR>', opts)
-map('n', '<Leader>vw', ':vsplit<CR>', opts)
-
---- Harpoon marking and accessing files
+map('n', '<Leader>hw', ':split<CR>', opts)   -- Horizontal split
+map('n', '<Leader>vw', ':vsplit<CR>', opts)  -- Vertical split
+-- Harpoon marking and accessing files
 map('n', '<Leader>H', ':lua require("harpoon.mark").add_file()<CR>', opts)
 map('n', '<Leader>fl', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
 map('n', '<Leader><Tab>', ':lua require("harpoon.ui").nav_next()<CR>', opts)
 
---- Window and split management
-map('n', '<Leader>nw', ':tabnew<CR>', opts)  -- Opens a new tab
+-- Window and split management
 map('n', '<Leader>hw', ':split<CR>', opts)   -- Horizontal split
 map('n', '<Leader>vw', ':vsplit<CR>', opts)  -- Vertical split
 map('n', '<Leader>l', '<C-w>l', opts)        -- Move to the right window
 map('n', '<Leader>h', '<C-w>h', opts)        -- Move to the left window
 map('n', '<Leader>j', '<C-w>j', opts)        -- Move to the lower window
 map('n', '<Leader>k', '<C-w>k', opts)        -- Move to the upper window
-map('n', '<Leader>w<Tab>', '<C-w>w', opts)   -- Cycle through windows
 
+-- Open LazyGit in a new split
+map('n', '<Leader>g', ':LazyGit<CR>', opts)
+ 
