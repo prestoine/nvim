dd34695dabcbdccdf9a7ccb870839dd187a1b5b0
prestoine <Preston@desouzafernandez.com>
2024-08-13 16:46:49 -0500

    autoformatting working

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -7,7 +7,10 @@ local function setup_formatting(client, bufnr)
             group = vim.api.nvim_create_augroup("Format_" .. bufnr, { clear = true }),
             buffer = bufnr,
             callback = function() 
-                vim.lsp.buf.format({ bufnr = bufnr })
+                vim.lsp.buf.format({ 
+                    bufnr = bufnr,
+                    timeout_ms = 5000  -- Set a 5-second timeout
+                })
             end
         })
     end
@@ -19,40 +22,53 @@ local on_attach = function(client, bufnr)
     -- Add any other on_attach functionality here
 end
 
+-- Common LSP setup options
+local lsp_flags = {
+    debounce_text_changes = 150,
+}
+
 -- Python
 lspconfig.pyright.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 
 -- JavaScript/TypeScript
 lspconfig.tsserver.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 
 -- Rust
 lspconfig.rust_analyzer.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 
 -- Go
 lspconfig.gopls.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 
 -- HTML, CSS, and JSON
 lspconfig.html.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 lspconfig.cssls.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 lspconfig.jsonls.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 
 -- Astro
 lspconfig.astro.setup{
-    on_attach = on_attach
+    on_attach = on_attach,
+    flags = lsp_flags,
 }
 
 -- Create a global autocommand group for LSP
@@ -67,3 +83,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
         on_attach(client, bufnr)
     end,
 })
+
+-- Preload LSP servers
+local preload_lsp_servers = {'astro', 'tsserver', 'html', 'cssls'}
+for _, server in ipairs(preload_lsp_servers) do
+    lspconfig[server].setup{}
+end
